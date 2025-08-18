d77d4963b4dad90ed4d2e787bdba34046ad33ccc
Marcos Btesh <68572576+marcosbtesh@users.noreply.github.com>
2025-08-18 11:10:14 -0300

    Update community.lua

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -8,5 +8,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
   -- import/override with your plugins folder
 }
