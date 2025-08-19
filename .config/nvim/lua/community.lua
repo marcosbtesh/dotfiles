8d22d6e757fae0058c45f2e6b1f6f38e17dffebf
Marcos Btesh <68572576+marcosbtesh@users.noreply.github.com>
2025-08-19 17:14:33 -0300

    added copilot completions to nvim

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -12,5 +12,6 @@ return {
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.completion.copilot-cmp" },
   -- import/override with your plugins folder
 }
