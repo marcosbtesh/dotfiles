e9e3595bf0d9d46d847b4bc186d6315a32cc8ae9
Marcos Btesh <68572576+marcosbtesh@users.noreply.github.com>
2025-10-24 17:21:05 -0300

    added another color scheme

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -21,6 +21,7 @@ return {
   { import = "astrocommunity.editing-support.vim-visual-multi" },
   { import = "astrocommunity.colorscheme.vscode-nvim" },
   { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.colorscheme.poimandres-nvim" },
   { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
   { import = "astrocommunity.scrolling.mini-animate" },
   { import = "astrocommunity.utility.hover-nvim" },
