60d4bd0aa5d8d8c35c102fb5cd32540888668171
Marcos Btesh <68572576+marcosbtesh@users.noreply.github.com>
2025-10-24 13:04:37 -0300

    better wezterm config

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@ set -e
 brew install nvim tmux gnupg stow fzf zoxide starship zsh-autosuggestions zsh-syntax-highlighting eza tmuxp ripgrep
 brew install --cask wezterm
 
+mkdir ~/.config/wezterm
+
 stow -t ~ zsh
 stow -t ~/.config .config
-stow -t ~ wezterm
+stow -t ~/.config/wezterm wezterm
 
 source ~/.zshrc
