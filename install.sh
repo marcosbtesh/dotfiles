d4744fc08455da3d93e5ec55fd4647fa0a9d58ce
Marcos Btesh <68572576+marcosbtesh@users.noreply.github.com>
2026-01-29 09:44:44 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 set -e
 
 brew tap browsh-org/homebrew-browsh
-brew install nvim tmux gnupg stow fzf zoxide starship zsh-autosuggestions zsh-syntax-highlighting eza tmuxp ripgrep w3m browsh
+brew install nvim tmux gnupg stow fzf zoxide starship zsh-autosuggestions zsh-syntax-highlighting eza tmuxp ripgrep w3m browsh newsboat
 
 brew install --cask wezterm
 
