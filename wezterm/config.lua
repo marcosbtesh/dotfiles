cd1761c5d8526e654c22d2c761d634e229e3d756
Marcos Btesh <68572576+marcosbtesh@users.noreply.github.com>
2026-01-29 22:56:25 -0300

    Update config.lua

--- wezterm/config.lua
+++ wezterm/config.lua
@@ -36,11 +36,11 @@ config.background = {
 	},
 	{
 		source = {
-			Color = "#282c35",
+			Color = "#000000",
 		},
 		width = "100%",
 		height = "100%",
-		opacity = 0,
+		opacity = 0.7,
 	},
 }
 config.window_background_opacity = 0
