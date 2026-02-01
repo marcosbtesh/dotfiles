8c784d3fe946a66e60da6b788304c1fd65d21f80
Marcos Btesh <68572576+marcosbtesh@users.noreply.github.com>
2026-02-01 17:54:09 -0300

    Update presence.lua

--- nvim/lua/plugins/presence.lua
+++ nvim/lua/plugins/presence.lua
@@ -17,7 +17,7 @@ return {
       git_commit_text = "Committing changes",
       plugin_manager_text = "Managing plugins",
       reading_text = "Reading %s",
-      -- workspace_text = "Working on (╯°□°)╯",
+      workspace_text = "Working on (╯°□°)╯",
       line_number_text = "Line %s out of %s",
     },
   },
