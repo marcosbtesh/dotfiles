485cbe7adc26a61a1eafa6f4f6391fed5826fe8e
Marcos Btesh <marcos@marcosbtesh.com>
2026-04-07 13:49:35 -0300

    astronvim_v6

---
+++ astronvim_v6/lua/community.lua
@@ -0,0 +1,43 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+-- return {
+--   "AstroNvim/astrocommunity",
+--   { import = "astrocommunity.pack.lua" },
+--   -- import/override with your plugins folder
+-- }
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.dart" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.html-css" },
+  -- { import = "astrocommunity.motion.nvim-surround" },
+  -- { import = "astrocommunity.completion.copilot-cmp" },
+  { import = "astrocommunity.editing-support.vim-visual-multi" },
+  -- { import = "astrocommunity.colorscheme.vscode-nvim" },
+  -- { import = "astrocommunity.colorscheme.rose-pine" },
+  -- { import = "astrocommunity.colorscheme.poimandres-nvim" },
+  -- { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  { import = "astrocommunity.scrolling.mini-animate" },
+  { import = "astrocommunity.utility.hover-nvim" },
+  { import = "astrocommunity.git.git-blame-nvim" },
+  { import = "astrocommunity.comment.ts-comments-nvim" },
+  -- { import = "astrocommunity.indent.indent-rainbowline" },
+  -- { import = "astrocommunity.indent.mini-indentscope" },
+  -- { import = "astrocommunity.file-explorer.fyler-nvim" },
+  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
+
+  -- import/override with your plugins folder
+  --
+  { import = "astrocommunity.media.cord-nvim" },
+}
---
+++ astronvim_v6/lua/plugins/mappings.lua
@@ -0,0 +1,69 @@
+-- lua/plugins/mappings.lua
+return {
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          -- Move line up/down (like VS Code Alt+Up/Alt+Down)
+          ["<A-Up>"] = { ":m .-2<CR>==", desc = "Move line up" },
+          ["<A-Down>"] = { ":m .+1<CR>==", desc = "Move line down" },
+          ["<A-k>"] = { ":m .-2<CR>==", desc = "Move line up (fallback)" },
+          ["<A-j>"] = { ":m .+1<CR>==", desc = "Move line down (fallback)" },
+
+          -- Copy line up/down (Shift+Alt)
+          ["<A-S-Up>"] = { ":t.-1<CR>==", desc = "Copy line up" },
+          ["<A-S-Down>"] = { ":t.<CR>==", desc = "Copy line down" },
+
+          -- delete/change without yanking
+          ["d"] = { '"_d', desc = "Delete without yanking" },
+          ["c"] = { '"_c', desc = "Change without yanking" },
+          ["x"] = { '"_x', desc = "Delete char without yanking" },
+          ["D"] = { '"_D', desc = "Delete to end of line without yanking" },
+          ["C"] = { '"_C', desc = "Change to end of line without yanking" },
+          ["s"] = { '"_s', desc = "Substitute char without yanking" },
+
+          -- keep yank versions
+          ["yd"] = { "d", desc = "Delete with yank" },
+          ["yc"] = { "c", desc = "Change with yank" },
+          ["yx"] = { "x", desc = "Delete char with yank" },
+          ["yD"] = { "D", desc = "Delete to end of line with yank" },
+          ["yC"] = { "C", desc = "Change to end of line with yank" },
+          ["ys"] = { "s", desc = "Substitute char with yank" },
+
+          -- undo / redo remaps
+          ["U"] = { "u", desc = "Undo last change" },
+          ["<Leader>r"] = { "<C-r>", desc = "Redo last undo" },
+        },
+
+        v = {
+          -- Move selected block up/down
+          ["<A-Up>"] = { ":m '<-2<CR>gv=gv", desc = "Move selection up" },
+          ["<A-Down>"] = { ":m '>+1<CR>gv=gv", desc = "Move selection down" },
+          ["<A-k>"] = { ":m '<-2<CR>gv=gv", desc = "Move selection up (fallback)" },
+          ["<A-j>"] = { ":m '>+1<CR>gv=gv", desc = "Move selection down (fallback)" },
+
+          -- Copy selection up/down (Shift+Alt)
+          ["<A-S-Up>"] = { ":t'<-1<CR>gv", desc = "Copy selection up" },
+          ["<A-S-Down>"] = { ":t'>+1<CR>gv", desc = "Copy selection down" },
+
+          -- delete/change without yanking
+          ["d"] = { '"_d', desc = "Delete without yanking" },
+          ["c"] = { '"_c', desc = "Change without yanking" },
+          ["x"] = { '"_x', desc = "Delete char without yanking" },
+          ["D"] = { '"_D', desc = "Delete to end of line without yanking" },
+          ["C"] = { '"_C', desc = "Change to end of line without yanking" },
+          ["s"] = { '"_s', desc = "Substitute char without yanking" },
+
+          -- keep yank versions
+          ["yd"] = { "d", desc = "Delete with yank" },
+          ["yc"] = { "c", desc = "Change with yank" },
+          ["yx"] = { "x", desc = "Delete char with yank" },
+          ["yD"] = { "D", desc = "Delete to end of line with yank" },
+          ["yC"] = { "C", desc = "Change to end of line with yank" },
+          -- ["ys"] = { "s", desc = "Substitute char with yank" },
+        },
+      },
+    },
+  },
+}
