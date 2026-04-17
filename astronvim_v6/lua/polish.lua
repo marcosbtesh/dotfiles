091fc18fa8b9a0620d5235d6cbd3f4413a6bd58f
Marcos Btesh <marcos@marcosbtesh.com>
2026-04-17 16:37:47 -0300

    Update polish.lua

--- astronvim_v6/lua/polish.lua
+++ astronvim_v6/lua/polish.lua
@@ -1,5 +1,7 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+-- Allow per-project .nvim.lua files (used for project-local DAP configs, etc.)
+vim.opt.exrc = true
+vim.opt.secure = true
